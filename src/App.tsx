628d2c4ef0a1d5e01e92b7a0b6eaf65bb7fecd03
BrackListed <cameta8900@gmail.com>
2026-05-07 19:25:39 +0800

    Styled the main page. Buttons still not functional for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import { Left } from "./assets/Left";
-
+import { BookPlus } from "lucide-react";
 export default function App(){
   return (
     <div className="flex gap-2-">
@@ -10,12 +10,31 @@ export default function App(){
   )
 }
 
+let tasks:string[] = []
+let addTask = document.getElementById('add-task')
+
 function Main() {
-  return null
+  return(
+    <div className="flex flex-col p-10 gap-10">
+      <h1 className="font-bold text-5xl text-amber-50 ">Today's Tasks: </h1>
+      <Taskcontainer/>
+    </div>
+  )
 }
 
-
-
-
+function Taskcontainer(){
+  return(
+    <div className= "flex flex-col p-2 w-2xl h-auto gap-2 rounded-2xl text-white" id = "add-task">
+      <form>
+        <div className="flex text-3xl items-center transition-transform hover:bg-zinc-600 cursor-pointer rounded-2xl w-fit hover:scale-110 hover:p-2 ">Add a task <BookPlus/></div>
+        <ul className="list-disc list-inside text-2xl">
+          <li>OI</li>
+          <li>Learn typescript and shiii</li>
+          <input className= "w-5xl bg-zinc-600 rounded-md outline-none focus-visible: ring-2 focus-visible: ring-violet-600"></input>
+        </ul>
+      </form>
+    </div>
+  )
+}
 
 
