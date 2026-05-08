aca222ed79754b424cc86fe7d13e65ffbc8223e2
BrackListed <cameta8900@gmail.com>
2026-05-08 19:03:20 +0800

    Now saves tasks and stores them.

--- src/assets/Taskcontainer.tsx
+++ src/assets/Taskcontainer.tsx
@@ -1,25 +1,37 @@
 import { BookPlus } from "lucide-react"
+import { useState } from "react"
 
-let tasks = [
-    {id: 1, name: "Get Practice with Typescript"},
-    {id: 2, name: "Get Practice with React"},
-    {id: 3, name: "Get Practice with Tailwind CSS"},
-]
 
 
 export function Taskcontainer(){
+    const [tasks, setTask]= useState(JSON.parse(localStorage.getItem("task-storage") ?? "[]"))
+    const [showInput, setShowInput] = useState(false)
   return(
     <div className= "flex flex-col p-2 w-2xl h-auto gap-2 rounded-2xl text-white" id = "add-task">
-      <form>
-        <div className="flex text-3xl items-center transition-transform hover:bg-zinc-600 cursor-pointer rounded-2xl w-fit hover:scale-110 hover:p-2 ">Add a task <BookPlus/></div>
+      <div className="flex flex-col gap-10">
+        <div className="flex text-3xl items-center transition-transform hover:bg-zinc-600 cursor-pointer rounded-2xl w-fit hover:scale-105 hover:p-2" onClick={() => setShowInput(true)}>
+            Add a task 
+            <BookPlus/>
+        </div>
+        {tasks.length === 0 && <h1 className = "text-2xl">No tasks yet. Add one to get started!</h1>}
         <ul className="list-disc list-inside text-2xl">
-            {tasks.map((task) => (
-                <li>{task.name}</li>
+            {tasks.map((userTask:string) => (
+                <li>{userTask}</li>
             ))}
-            <input className= "w-5xl bg-zinc-600 rounded-md outline-none focus-visible: ring-2 focus-visible: ring-violet-600"></input>
+            {showInput && <input className= "w-5xl bg-zinc-600 rounded-md outline-none focus-visible: ring-2 focus-visible: ring-violet-600" onKeyDown = {(e) =>{
+                if(e.key === "Enter"){
+                    let newTask = (e.target as HTMLInputElement).value
+                    setTask([...tasks, newTask])
+                    localStorage.setItem("task-storage", JSON.stringify(tasks))
+                } 
+            }} onBlur={() => {
+                console.log("Clicked off??")
+            }}></input>}
         </ul>
         
-      </form>
+        
+      </div>
     </div>
   )
+
 }
