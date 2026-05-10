69e20379ee55a9cd72f960ccaa8f5d1b1a6f84c0
BrackListed <cameta8900@gmail.com>
2026-05-10 14:33:45 +0800

    Made hovering over buttons more smooth

--- src/assets/Panels.tsx
+++ src/assets/Panels.tsx
@@ -2,17 +2,17 @@ import {Notebook, Calendar, RefreshCcw} from 'lucide-react';
 export function Panels(){
   return(
     <div className="flex flex-col gap-3 p-3">
-      <div className="flex gap-3 transition-transform hover:bg-gray-200 hover:scale-110 rounded-md hover: cursor-pointer">
+      <div className="flex gap-3 transition-all hover:bg-gray-200 hover:scale-110 rounded-md hover: cursor-pointer hover:p-2">
         <Notebook/>
         <a href = "" target = "_blank" rel = "noreferrer noopener">Plans</a>
       </div>
 
-      <div className="flex gap-3 transition-transform hover:bg-gray-200 hover:scale-110 rounded-md hover:cursor-pointer">
+      <div className="flex gap-3 transition-all hover:bg-gray-200 hover:scale-110 rounded-md hover:cursor-pointer hover:p-2">
         <Calendar/>
         <a href = "" target = "_blank" rel = "noreferrer noopener">Calendar</a>
       </div>
 
-      <div className="flex gap-3 transition-transform hover:bg-gray-200 hover:scale-110 rounded-md hover:cursor-pointer" >
+      <div className="flex gap-3 transition-all hover:bg-gray-200 hover:scale-110 rounded-md hover:cursor-pointer hover:p-2" >
         <RefreshCcw/>
         <a href = "" target = "_blank" rel = "noreferrer noopener">Habits</a>
       </div>
--- src/assets/Taskcontainer.tsx
+++ src/assets/Taskcontainer.tsx
@@ -15,15 +15,15 @@ export function Taskcontainer(){
   return(
     <div className= "flex flex-col p-2 w-2xl h-auto gap-2 rounded-2xl text-white" id = "add-task">
       <div className="flex flex-col gap-10">
-        <div className="flex text-3xl items-center transition-transform hover:bg-zinc-600 cursor-pointer rounded-2xl w-fit hover:scale-105 hover:p-2" onClick={() => setShowInput(true)}>
+        <div className="flex text-3xl items-center transition-all hover:bg-zinc-600 cursor-pointer rounded-2xl w-fit hover:scale-105 hover:p-2" onClick={() => setShowInput(true)}>
             Add a task 
             <BookPlus/>
         </div>
         {tasks.length === 0 && <h1 className = "text-2xl">No tasks yet. Add one to get started!</h1>}
         <ul className="list-disc list-inside text-2xl">
             {tasks.map((userTask) => (
-                <li className="flex w-fit items-center hover:bg-zinc-700 hover:rounded-md hover:p-2  hover: scale-105transition-transform gap-3">{userTask.name} 
-                        <button onClick = {() => RemoveTask(userTask.id)} className="w-fit text-zinc-400 hover:scale-105 transition-transform hover:cursor-pointer rounded-lg hover:bg-zinc-500">×</button>
+                <li className="flex w-fit items-center hover:bg-zinc-700 hover:rounded-md hover:p-2  hover: scale-105 transition-all gap-3">{userTask.name} 
+                        <button onClick = {() => RemoveTask(userTask.id)} className="w-fit text-zinc-400 hover:scale-105 transition-all hover:cursor-pointer rounded-lg hover:bg-zinc-500">×</button>
                 </li>
             ))}
             {showInput && <input ref = {inputRef} className= "w-5xl bg-zinc-600 rounded-md outline-none focus-visible: ring-2 focus-visible: ring-violet-600" onKeyDown = {(e) =>{
