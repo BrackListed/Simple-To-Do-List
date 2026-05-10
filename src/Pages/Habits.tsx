13629671df2763c094482aa7e842387ce76079e6
BrackListed <cameta8900@gmail.com>
2026-05-10 18:43:50 +0800

    Can now add habits

--- src/Pages/Habits.tsx
+++ src/Pages/Habits.tsx
@@ -1,20 +1,49 @@
 import { HabitButtons } from "../assets/HabitButtons";
+import { useRef, useState } from "react";
 
 export function Habits() {
+    type habit = {
+        id: number;
+        name: string;
+    }
+    const [habits, setHabit] = useState<habit[]>(JSON.parse(localStorage.getItem("user-habits")?? "null") ?? [])
+    const habitRef = useRef<HTMLInputElement>(null)
     return(
         //Add a habit, like "Practice react, and be able to tick boxes for the entire week.
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center gap-5">
             <h1 className="text-center text-4xl text-zinc-50 font-bold my-5">Your Daily Habits</h1>
             <div id = "add-habit" className="flex my-5 gap-3 items-center">
-                <input placeholder= "Input habit here.." className=" p-2 outline-none bg-zinc-700 w-96 h-10 rounded-lg text-zinc-50 focus-visible:ring-2 focus-visible:ring-violet-600"></input>
-                <HabitButtons addTask={addTask}>Add Habit</HabitButtons>
-            </div>
+                <input ref = {habitRef} placeholder= "Input habit here.." className=" p-2 outline-none bg-zinc-700 w-96 h-10 rounded-lg text-zinc-50 focus-visible:ring-2 focus-visible:ring-violet-600"></input>
+                <HabitButtons addHabit={addHabit}>Add Habit</HabitButtons>
+             </div>
+            {habits.length === 0 && <h1 className="text-zinc-50 font-semibold text-center">You have no habits currently. Add one to get started!</h1>}
+                {habits.map((habit) => (
+                    <div className="flex flex-col bg-zinc-700 rounded-lg w-2xl justify-center text-zinc-50 p-2">
+                        <div id = "header" className="flex justify-between items-center ">
+                            {habit.name}
+                            <HabitButtons delHabit={() => deleteHabit(habit.id)}>Delete</HabitButtons>
+                        </div>
+
+                        <div>
+
+
+                        </div>
+
+                    </div>
+                ))}
         </div>
     )
 
-    function addTask() {
-        console.log("Hallo world!")
+    function addHabit() {
+        let userHabit = habitRef.current!.value
+        let newHabit = {id: Date.now(), name: userHabit }
+        const updatedHabits = [...habits, newHabit]
+        setHabit(updatedHabits)
+        localStorage.setItem("user-habits", JSON.stringify(updatedHabits))
     }
 }
 
+function deleteHabit(id: number){
+    console.log(id)
+}
 
--- src/assets/HabitButtons.tsx
+++ src/assets/HabitButtons.tsx
@@ -2,12 +2,13 @@ import type { ReactNode } from "react"
 
 type HabitButtonProp = {
     children: ReactNode
-    addTask: () => void
+    addHabit?: () => void
+    delHabit?: () => void
 }
 
-export function HabitButtons({children, addTask}: HabitButtonProp) {
+export function HabitButtons({children, addHabit, delHabit}: HabitButtonProp) {
     return(
-        <button onClick = {() => addTask()} className="bg-violet-600 p-2 rounded-lg text-zinc-50 transition-all hover:cursor-pointer hover:scale-110 hover:bg-violet-500 ">{children}</button>
+        <button onClick = {() => addHabit?.() || delHabit?.()} className="bg-violet-600 p-2 rounded-lg text-zinc-50 transition-all hover:cursor-pointer hover:scale-110 hover:bg-violet-500 ">{children}</button>
     )
 
 
