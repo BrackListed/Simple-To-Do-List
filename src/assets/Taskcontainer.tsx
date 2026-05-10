1f203bebfc22f254c5e7956926595e6176a21f8f
BrackListed <cameta8900@gmail.com>
2026-05-10 14:26:15 +0800

    You can now remove individual tasks by clicking the X button right next to the tasks.

--- src/assets/Taskcontainer.tsx
+++ src/assets/Taskcontainer.tsx
@@ -1,12 +1,17 @@
-import { BookPlus } from "lucide-react"
+import { BookPlus, Filter } from "lucide-react"
 import { useRef, useState } from "react"
 
 
 
 export function Taskcontainer(){
-    const [tasks, setTask]= useState(JSON.parse(localStorage.getItem("task-storage") ?? "[]"))
+    const [tasks, setTask]= useState<userTask[]>(JSON.parse(localStorage.getItem("task-storage") ?? "[]"))
     const [showInput, setShowInput] = useState(false)
     const inputRef = useRef<HTMLInputElement>(null)
+    type userTask = {
+        id: number;
+        name: string;
+    }
+
   return(
     <div className= "flex flex-col p-2 w-2xl h-auto gap-2 rounded-2xl text-white" id = "add-task">
       <div className="flex flex-col gap-10">
@@ -16,12 +21,15 @@ export function Taskcontainer(){
         </div>
         {tasks.length === 0 && <h1 className = "text-2xl">No tasks yet. Add one to get started!</h1>}
         <ul className="list-disc list-inside text-2xl">
-            {tasks.map((userTask:string) => (
-                <li>{userTask}</li>
+            {tasks.map((userTask) => (
+                <li className="flex w-fit items-center hover:bg-zinc-700 hover:rounded-md hover:p-2  hover: scale-105transition-transform gap-3">{userTask.name} 
+                        <button onClick = {() => RemoveTask(userTask.id)} className="w-fit text-zinc-400 hover:scale-105 transition-transform hover:cursor-pointer rounded-lg hover:bg-zinc-500">×</button>
+                </li>
             ))}
             {showInput && <input ref = {inputRef} className= "w-5xl bg-zinc-600 rounded-md outline-none focus-visible: ring-2 focus-visible: ring-violet-600" onKeyDown = {(e) =>{
                 if(e.key === "Enter"){
-                    let newTask = (e.target as HTMLInputElement).value
+                    let taskContent = (e.target as HTMLInputElement).value
+                    let newTask = {id: Date.now(), name: taskContent}
                     const update = [...tasks, newTask]
                     setTask(update)
                     localStorage.setItem("task-storage", JSON.stringify(update))
@@ -32,9 +40,10 @@ export function Taskcontainer(){
             }}></input>}
         </ul>
         
-        
       </div>
     </div>
   )
-
+  function RemoveTask(id: number){
+    setTask(tasks.filter(task => task.id !== id))
+  }
 }
