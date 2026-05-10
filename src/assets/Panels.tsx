a3a28e5592402c2507333b7bf3e111932eabe4a3
BrackListed <cameta8900@gmail.com>
2026-05-10 17:35:16 +0800

    No longer have to directly click the text to be redirected to the page

--- src/assets/Panels.tsx
+++ src/assets/Panels.tsx
@@ -3,20 +3,18 @@ import { Link } from 'react-router-dom';
 export function Panels(){
   return(
     <div className="flex flex-col gap-3 p-3">
-      <div className="flex gap-3 transition-all hover:bg-gray-200 hover:scale-110 rounded-md hover: cursor-pointer hover:p-2">
-        <Notebook/>
-        <Link to ="/Plans">Plans</Link>
+      <div>
+        <Link to ="/Calendar" className="flex gap-3 transition-all hover:bg-gray-200 hover:scale-110 rounded-md hover:cursor-pointer hover:p-2"><Notebook/>Plans</Link>
       </div>
 
-      <div className="flex gap-3 transition-all hover:bg-gray-200 hover:scale-110 rounded-md hover:cursor-pointer hover:p-2">
-        <Calendar/>
-        <Link to ="/Calendar">Calendar</Link>
+      <div>
+        <Link to ="/Calendar" className="flex gap-3 transition-all hover:bg-gray-200 hover:scale-110 rounded-md hover:cursor-pointer hover:p-2"><Calendar/>Calendar</Link>
       </div>
 
-      <div className="flex gap-3 transition-all hover:bg-gray-200 hover:scale-110 rounded-md hover:cursor-pointer hover:p-2" >
-        <RefreshCcw/>
-        <Link to ="/Habits">Habits</Link>
+      <div >
+        <Link to ="/Habits" className="flex gap-3 transition-all hover:bg-gray-200 hover:scale-110 rounded-md hover:cursor-pointer hover:p-2"><RefreshCcw/>Habits</Link>
       </div>
+      
     </div>
   )
 }
